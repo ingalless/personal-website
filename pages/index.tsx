bcab1ed4378e7c902267e6f2a965136a327b75a4
Jonathan <jonny@mondago.com>
2021-03-22 13:43:09 +0000

    More fun

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,9 +5,9 @@ export default function Home() {
   return (
     <Layout>
       <div className="font-mono w-full">
-        <ul className="bg-gray-50 border border-gray-300 rounded p-4 font-semibold">
+        <ul className="bg-gray-50 border border-gray-300 rounded p-4 font-semibold group">
           <h2 className="hidden uppercase font-mono">Projects</h2>
-          <p>
+          <p className="opacity-100 group-hover:opacity-70 transition-opacity">
             <span className="text-green-700">foreach</span> (
             <span className="text-green-500">$projects</span> as{" "}
             <span className="text-green-500">$project</span>):
@@ -25,7 +25,7 @@ export default function Home() {
               foveal.dev
             </a>
           </li>
-          <p>
+          <p className="opacity-100 group-hover:opacity-70 transition-opacity">
             <span className="text-green-700">endforeach;</span>
           </p>
         </ul>
